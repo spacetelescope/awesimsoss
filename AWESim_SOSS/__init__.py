24fed959983b920c15778c3ce3b1c4fd74e0fe10
ExoWanderer <jdfraine@gmail.com>
2017-10-18 20:24:29 -0400

    Created New Master to Punt on Learning How to Merge

--- AWESim_SOSS/__init__.py
+++ AWESim_SOSS/__init__.py
@@ -1,4 +1,18 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# Packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+if not _ASTROPY_SETUP_:
+    # For egg_info test builds to pass, put package imports here.
+
+    from .example_mod import *
+
 from . import extract1D
 from . import lcfitting
 from . import structSOSS
 from . import synSOSS
+from . import example_mod
