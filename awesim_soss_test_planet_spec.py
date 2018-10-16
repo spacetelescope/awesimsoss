881b1702b47b8976d2517c1e669ba59f30aa0014
Jonathan Fraine <exowanderer@users.noreply.github.com>
2018-10-16 17:44:32 -0400

    Converted input time to days from seconds

--- awesim_soss_test_planet_spec.py
+++ awesim_soss_test_planet_spec.py
@@ -7,6 +7,9 @@ import astropy.units as q
 import astropy.constants as ac
 import batman
 from pkg_resources import resource_filename
+
+day2sec = 86400
+
 star = np.genfromtxt(resource_filename('AWESim_SOSS','files/scaled_spectrum.txt'), unpack=True)
 star1D = [star[0]*q.um, (star[1]*q.W/q.m**2/q.um).to(q.erg/q.s/q.cm**2/q.AA)]
 
@@ -26,7 +29,7 @@ params.ecc = 0.                               # eccentricity
 params.w = 90.                                # longitude of periastron (in degrees)
 params.limb_dark = 'quadratic'                # limb darkening profile to use
 params.u = [0.1,0.1]                          # limb darkening coefficients
-tmodel = batman.TransitModel(params, tso.time)
+tmodel = batman.TransitModel(params, tso.time/day2sec)
 tmodel.teff = 3500                            # effective temperature of the host star
 tmodel.logg = 5                               # log surface gravity of the host star
 tmodel.feh = 0                                # metallicity of the host star
