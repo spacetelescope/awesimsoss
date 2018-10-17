4a8e8827468e0cef05989202efc06af95230ca2e
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-10-17 14:04:37 -0400

    Switched to current ExoCTK package until exoctk rename.

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 setup(name='awesimsoss',
       version=0.1,
       description='Advanced Webb Exposure Simulator for SOSS',
-      install_requires=['astropy', 'scipy', 'matplotlib', 'numpy', 'batman-package', 'exoctk'],
+      install_requires=['astropy', 'scipy', 'matplotlib', 'numpy', 'batman-package', 'ExoCTK'],
       author='Joe Filippazzo and Jonathan Fraine',
       author_email='jfilippazzo@stsci.edu',
       license='MIT',
-      url='https://github.com/ExoCTK/AWESim_SOSS',
+      url='https://github.com/spacetelescope/awesimsoss',
       long_description='AWESim_SOSS is a TSO simulator for the Single Object Slitless Spectroscopy mode of the NIRISS instrument onboard JWST',
       zip_safe=False,
       use_2to3=False
