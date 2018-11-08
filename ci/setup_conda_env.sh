7b8e28a03559cbaa8be870d625e7f057f857ea5e
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-11-08 14:27:55 -0500

    exoctk import

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -5,5 +5,5 @@ conda create -n awesimsoss python=$PYTHON_VERSION || exit 1
 source activate awesimsoss
 
 echo "Installing packages..."
-conda install flake8 beautifulsoup4 lxml numpy astropy
+conda install flake8 beautifulsoup4 lxml numpy astropy exoctk
 pip install exoctk pytest pytest-cov coveralls
