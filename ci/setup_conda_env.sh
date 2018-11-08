842a2f9035e40b407c13b018b087e36edb360aef
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-11-07 21:18:12 -0500

    Added coveralls

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -6,5 +6,4 @@ source activate awesimsoss
 
 echo "Installing packages..."
 conda install flake8 beautifulsoup4 lxml numpy astropy
-# git clone https://github.com/ExoCTK/ExoCTK.git
-# python ExoCTK/setup.py develop
+pip install pytest pytest-cov coveralls
