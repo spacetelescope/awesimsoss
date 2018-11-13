648c83f6b8c888491ba32756c14cebc485b6949d
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-11-13 09:59:19 -0500

    Updated setup.py

--- awesimsoss/__init__.py
+++ awesimsoss/__init__.py
@@ -1,1 +1,6 @@
 from .awesim import TSO
+from . import awesim
+from . import generate_darks
+from . import make_trace
+from . import noise_niriss
+from . import noise_simulation
--- setup.py
+++ setup.py
@@ -1,15 +1,36 @@
-#!/usr/bin/env python
-from setuptools import setup
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+from codecs import open
+from os import path
 
 setup(name='awesimsoss',
-      version=0.1,
+      version='0.1.0',
       description='Advanced Webb Exposure Simulator for SOSS',
-      install_requires=['astropy', 'scipy', 'matplotlib', 'numpy', 'batman-package', 'exoctk'],
-      author='Joe Filippazzo and Jonathan Fraine',
+      long_description='AWESim_SOSS is a TSO simulator for the Single Object Slitless Spectroscopy mode of the NIRISS instrument onboard JWST',
+      url='https://github.com/spacetelescope/awesimsoss',
+      author='Joe Filippazzo',
       author_email='jfilippazzo@stsci.edu',
       license='MIT',
-      url='https://github.com/spacetelescope/awesimsoss',
-      long_description='AWESim_SOSS is a TSO simulator for the Single Object Slitless Spectroscopy mode of the NIRISS instrument onboard JWST',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+      ],
+      keywords='astrophysics',
+      packages=find_packages(exclude=['contrib', 'docs']),
+      package_data={'svo_filters': ['data/filters/*', 'data/plots/*', 'data/spectra/*']},
+      include_package_data=True,
       zip_safe=False,
+      install_requires=['astropy', 'scipy', 'matplotlib', 'numpy', 'batman-package', 'exoctk'],
       use_2to3=False
+
 )
