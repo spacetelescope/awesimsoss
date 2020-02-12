e99892cd7e86c0b676e58c013ed5e66ce3ce3943
Nestor Espinoza <nespinoza@espino.stsci.edu>
2020-02-12 16:07:53 -0500

    Made ATLAS model default ones.

--- awesimsoss/awesim.py
+++ awesimsoss/awesim.py
@@ -1689,7 +1689,7 @@ class ModelTSO(TSO):
         return f*scaling_factor
 
     """Generate a test object with a blackbody spectrum"""
-    def __init__(self, ngrps=2, nints=2, teff=5700.0, logg = 4.0, feh = 0.0, vturb = 2.0, alpha = 0.0, jmag = 9.0, stellar_model = 'PHOENIX', filter='CLEAR', subarray='SUBSTRIP256', run=True, add_planet=False, scale=1., **kwargs):
+    def __init__(self, ngrps=2, nints=2, teff=5700.0, logg = 4.0, feh = 0.0, vturb = 2.0, alpha = 0.0, jmag = 9.0, stellar_model = 'ATLAS', filter='CLEAR', subarray='SUBSTRIP256', run=True, add_planet=False, scale=1., **kwargs):
         """Get the test data and load the object
 
         Parmeters
